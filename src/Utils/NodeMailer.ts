b7251986f9ccb3d43bc2b469855a4549b0df2c47
Rishabh Mishra <42122068+rishabhgithub123@users.noreply.github.com>
2021-04-25 20:16:51 +0530

    Update NodeMailer.ts

--- src/Utils/NodeMailer.ts
+++ src/Utils/NodeMailer.ts
@@ -5,7 +5,7 @@ export class NodeMailer {
     private static initilizerTansport(){
         return nodeMailer.createTransport(SendGrid({
             auth: {
-                api_key: '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'
+                api_key: '#########################################################'
             }
         }))
     }
